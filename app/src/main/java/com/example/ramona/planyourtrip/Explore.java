b738e075ce8b51f1a6a267ab916b13336565110f
iancudan <iancu_dansilviu@yahoo.ro>
2018-01-28 12:41:34 +0200

    Multi Language v2 Navigation View

--- app/src/main/java/com/example/ramona/planyourtrip/Explore.java
+++ app/src/main/java/com/example/ramona/planyourtrip/Explore.java
@@ -1,8 +1,6 @@
 package com.example.ramona.planyourtrip;
 
-import android.content.Context;
 import android.content.Intent;
-import android.content.res.Resources;
 import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
@@ -10,41 +8,29 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.NavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.ramona.planyourtrip.MultiLanguage.Language;
-import com.example.ramona.planyourtrip.MultiLanguage.MultiLanguageHelper;
 
-import java.util.ArrayList;
-import java.util.List;
+import static com.example.ramona.planyourtrip.Travel.listaIDTextViews;
 
-import io.paperdb.Paper;
-
-public class Travel extends AppCompatActivity {
+public class Explore extends AppCompatActivity {
 
     Language language = new Language();
-    public static List<Integer> listaIDTextViews = new ArrayList<>();
-    TextView textView ;
-    TextView textViewNavHome ;
-    TextView textViewNavProfile ;
-    TextView textViewNavExplore ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_travel);
+        setContentView(R.layout.activity_explore);
         allYouNeed();
     }
-
     private void allYouNeed() {
         //preiau toate textViewurile din clasa
-        textView = (TextView) findViewById(R.id.textView);
+       // ex: textView = (TextView) findViewById(R.id.textView);
         //pun toate id-urile stringurilor de care am nevoie
         getAllIdTextView();
-        //seteaza limba default
-        language.setDefaultLanguage(this,textView);
         //navigation view
         navView();
     }
@@ -83,26 +69,9 @@ public class Travel extends AppCompatActivity {
         Intent a = new Intent(this,intent);
         startActivity(a);
     }
-
     private void getAllIdTextView() {
-        Integer idTextView = R.string.hello;
+        //ex:Integer idTextView = R.string.hello;
         //adauga in lista de traduceri
-        listaIDTextViews.add(idTextView);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_activity,menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if(item.getItemId()== R.id.engleza){
-           language.schimbaLimba(this,"EN",textView);
-        }else if(item.getItemId()== R.id.romana){
-            language.schimbaLimba(this,"RO",textView);
-        }
-        return super.onOptionsItemSelected(item);
+        //ex: listaIDTextViews.add(idTextView);
     }
 }
