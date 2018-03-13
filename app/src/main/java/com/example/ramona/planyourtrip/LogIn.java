1d2dbe06be91b54aac2ca8671f81762b4b90cb88
iancudan <iancu_dansilviu@yahoo.ro>
2018-03-13 19:52:55 +0200

    F120-SignUp

--- app/src/main/java/com/example/ramona/planyourtrip/LogIn.java
+++ app/src/main/java/com/example/ramona/planyourtrip/LogIn.java
@@ -1,7 +1,10 @@
 package com.example.ramona.planyourtrip;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LogIn extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class LogIn extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+    }
+
+    //deschide SignUp Form
+    public void deschideSignUp(View View){
+        Intent intent= new Intent(LogIn.this, SignUpActivity.class);
+        startActivity(intent);
     }
 }
